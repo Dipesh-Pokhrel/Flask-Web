8019700725606a89c8fb358f57c5753ee88f21f3
Dipesh-Pokhrel <dipeshpokhrel45@gmail.com>
2022-04-06 13:23:57 +0545

    forms in flask

---
+++ flask_form/flash.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, session, redirect, url_for,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask (__name__)
+
+app.config['SECRET_KEY'] =  'my_key'
+
+class SimpleForm(FlaskForm):
+    breed = StringField('What breed are you ?')
+    submit = SubmitField('Click Me')
+
+@app.route('/', methods=["GET","POST"])
+def flash_form():
+    form = SimpleForm()
+
+    if form.validate_on_submit():
+        session['breed'] = form.breed.data
+        flash(f"You just changed your breed to:{session['breed']}")
+        return redirect(url_for('flash_form'))
+    return render_template('flash.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)

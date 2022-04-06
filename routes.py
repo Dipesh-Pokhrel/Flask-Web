28e810357249f9e3ae80a113c2e2f153222ef644
Dipesh-Pokhrel <dipeshpokhrel45@gmail.com>
2022-04-06 13:27:42 +0545

    routes in flask

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Creating an application object.
+app = Flask (__name__)
+
+
+@app.route('/') # 127.0.0.1:500
+def index():
+    return '<h1> Hello Puppy !</h1>'
+@app.route('/information') # 127.0.0.1:5000/information
+def info():
+    return '<h1> Puppies are cute ! </h1>'
+
+if __name__ == '__main__':
+    app.run()
